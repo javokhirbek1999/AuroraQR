85d237545815c438c9cfff35c62511e2165c6f1c
Faxriddin <fabdullajonov2003@gmail.com>
2022-08-25 20:34:11 +0300

    Added NewProject

--- bot/views.py
+++ bot/views.py
@@ -1,21 +1,21 @@
-import threading
-from django.views.generic import TemplateView
+# import threading
+# from django.views.generic import TemplateView
 
 
-from .telegramBot import *
+# from .telegramBot import *
 
-def setupBotThread():
-    """
-    Setup the thread for running the bot in background 
-    """
+# def setupBotThread():
+#     """
+#     Setup the thread for running the bot in background 
+#     """
 
-    botThread = threading.Thread(target=main)
+#     botThread = threading.Thread(target=main)
     
-    botThread.start()
+#     botThread.start()
     
 
-class HomeView(TemplateView):
-    setupBotThread()
-    template_name = 'bot/home.html'
+# class HomeView(TemplateView):
+#     setupBotThread()
+#     template_name = 'bot/templates/home/home.html'
 
 
