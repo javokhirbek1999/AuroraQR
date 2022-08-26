a22009c70898d6e38fee6719bf3d55d8f996f60b
Faxriddin <fabdullajonov2003@gmail.com>
2022-08-27 02:16:29 +0300

    Added views

--- bot/urls.py
+++ bot/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import HomeView
+from . import views
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    # path('', home, name='home'),
+    path('home/', views.HomeView.as_view(), name='home'),
+    path('generateQr/', views.GenerateQrView.as_view(), name='generateQr' ),
+    path('qrCode/', views.QrCodeView.as_view(), name='qrCode' ),
+    path('qrColor/', views.QrColorView.as_view(), name='qrColor' ),
+    path('about/', views.AboutView.as_view(), name='about' ),
+    path('help/', views.HelpView.as_view(), name='help' ),
 ]
--- bot/views.py
+++ bot/views.py
@@ -1,21 +1,45 @@
-# import threading
-# from django.views.generic import TemplateView
+import threading
+from django.views.generic import TemplateView, View
 
 
-# from .telegramBot import *
+from .telegramBot import *
 
-# def setupBotThread():
-#     """
-#     Setup the thread for running the bot in background 
-#     """
+def setupBotThread():
+    """
+    Setup the thread for running the bot in background 
+    """
 
-#     botThread = threading.Thread(target=main)
+    botThread = threading.Thread(target=main)
     
-#     botThread.start()
+    botThread.start()
     
 
-# class HomeView(TemplateView):
-#     setupBotThread()
-#     template_name = 'bot/templates/home/home.html'
+class HomeView(TemplateView):
+    setupBotThread()
+    template_name = 'main.html'
+
+class GenerateQrView(TemplateView):
+    setupBotThread()
+    template_name = 'generateQr.html'
+
+class QrCodeView(TemplateView):
+    setupBotThread()
+    template_name = 'QRcode.html'
+
+class QrColorView(TemplateView):
+    setupBotThread()
+    template_name = 'QRcolor.html'
+
+class AboutView(TemplateView):
+    setupBotThread()
+    template_name = 'about.html'
+
+    def get_template_names(self):
+        return super().get_template_names()
+
+class HelpView(TemplateView):
+    setupBotThread()
+    template_name = 'help.html'
+
 
 
